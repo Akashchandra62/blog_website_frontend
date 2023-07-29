3a2ef9a1e92032c241903291dabe91857053f993
Akashchandra62 <akashchandra8544@gmail.com>
2023-07-29 14:08:59 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api/api.js";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", inputs, {
+        withCredentials: true,
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.log(error.response);
+      setError(error.response.data);
+    }
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Login</h1>
       <form action="">
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
-        <button>Login</button>
-        <p>This is an Error</p>
-        <span>Don't you have an account? <Link to="/register">Register</Link></span>
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          placeholder="username"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err.message}</p>}
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api/api.js";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(inputs.password);
+      await api.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Register</h1>
       <form action="">
-        <input required type="text" placeholder='username' />
-        <input required type="email" placeholder='email' />
-        <input required type="password" placeholder='password' />
-        <button>Register</button>
-        <p>This is an Error</p>
-        <span>Do you have an account? <Link to="/login">Login</Link></span>
+        <input
+          required
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}> Register</button>
+        {err && <p>{err.message}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
